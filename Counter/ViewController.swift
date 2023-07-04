cc88d38a5c716ca5cab71d0e655b0b160e24b74f
79137203595 <79137203595@yandex.ru>
2023-07-04 12:19:28 +0300

    Добавление счетчика

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var UIButton: UIButton!
+    @IBOutlet weak var UILabel: UILabel!
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    var hir=1
+    
+    @IBAction func UIButton(_ sender: Any) {
+        UILabel.text = "Значение счётчика:\(hir)"
+        
+        hir = hir+1
+    }
+    
 }
 
